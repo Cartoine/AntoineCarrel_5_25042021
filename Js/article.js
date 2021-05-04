3a3b72fbde7187b8ed9ce084f51a41192aa672c0
Cartoine <antoine.carrel@orange.fr>
2021-05-04 22:34:01 +0200

    dynamique product page

--- Js/article.js
+++ Js/article.js
@@ -1,23 +1,71 @@
-// async function main() {
+const description = document.querySelectorAll('.cameraDescription')
+const imageUrl = document.querySelectorAll('.imageUrl')
+const lenses = document.querySelectorAll('.lenses')
+const cameraName = document.querySelectorAll('.cameraName');
+const prices = document.querySelectorAll('.cameraPrices');
+let resultFromAPI;
+// const btnCard = function()
+// {
+//     let id = this.id
+//     localStorage.id = this.id
+async function taMere(){
 
-//     const array_cameras = await fetch("http://localhost:3000/api/cameras")
-//     .then(resultat => resultat.json())
-//     .then(json => json)        
-//         // .then(json => json = json.forEach(function(product) {
-//         //     console.log(product)
-//         // }))
-//         displayCameraInfos(array_cameras)
-//         console.table(array_cameras)
-// }
 
-// function displayCameraInfos(data){
-//     const title = data[0].name;
-//     const description = data[0].description;
-//     const price = data[0].price;
-//     const image = data[0].imageUrl;
-    
-//     document.querySelector('#title').textContent = title
-//     document.querySelector('#description').textContent = description
-//     document.querySelector('#price').textContent = price
-//     document.querySelector('#image').src = image
-// }
+
+    await fetch(`http://localhost:3000/api/cameras`)
+        .then(responce => responce.json())
+        .then(data => {
+            resultFromAPI = data
+            console.table(data[1])
+
+            // console.table(resultFromAPI[1])
+            console.log(localStorage.id)
+            //probleme viens de la recupération des data de L'API
+
+            if(localStorage.id === "cardOne") {
+                
+                    description[0].innerText = resultFromAPI[0].description;
+                    imageUrl[0].src = resultFromAPI[0].imageUrl;
+                    cameraName[0].innerText = resultFromAPI[0].name;
+                    prices[0].innerText = `${resultFromAPI[0].price.toString().slice(0, -2)},00 € `;
+                
+                } else
+                 if(localStorage.id === "cardTwo"){
+
+                    description[0].innerText = resultFromAPI[1].description;
+                    imageUrl[0].src = resultFromAPI[1].imageUrl;
+                    cameraName[0].innerText = resultFromAPI[1].name;
+                    prices[0].innerText = `${resultFromAPI[1].price.toString().slice(0, -2)},00 € `;
+
+                } else
+                 if(localStorage.id === "cardThree"){
+
+                    description[0].innerText = resultFromAPI[2].description;
+                    imageUrl[0].src = resultFromAPI[2].imageUrl;
+                    cameraName[0].innerText = resultFromAPI[2].name;
+                    prices[0].innerText = `${resultFromAPI[2].price.toString().slice(0, -2)},00 € `;
+                   
+                } else
+                 if(localStorage.id === "cardFour"){
+
+                    description[0].innerText = resultFromAPI[3].description;
+                    imageUrl[0].src = resultFromAPI[3].imageUrl;
+                    cameraName[0].innerText = resultFromAPI[3].name;
+                    prices[0].innerText = `${resultFromAPI[3].price.toString().slice(0, -2)},00 € `;
+                
+                } else
+                 if(localStorage.id === "cardFive"){
+
+                    description[0].innerText = resultFromAPI[4].description;
+                    imageUrl[0].src = resultFromAPI[4].imageUrl;
+                    cameraName[0].innerText = resultFromAPI[4].name;
+                    prices[0].innerText = `${resultFromAPI[4].price.toString().slice(0, -2)},00 € `;
+                
+                } else {
+                    console.log("contacter l'équipe")
+                }
+            
+        })
+
+    }
+    taMere()
--- Js/index.js
+++ Js/index.js
@@ -5,26 +5,36 @@ const cameraName = document.querySelectorAll('.cameraName');
 const prices = document.querySelectorAll('.cameraPrices');
 let resultFromAPI;
 
- async function CallAPI(){
+async function CallAPI() {
     await fetch(`http://localhost:3000/api/cameras`)
-    .then(responce => responce.json())
-    .then(data => {
+        .then(responce => responce.json())
+        .then(data => {
 
-        resultFromAPI = data
-        // console.log(resultFromAPI)
-        for(let i in resultFromAPI){
-            // console.log(resultFromAPI[i]);
-            description[i].innerText = resultFromAPI[i].description;
-            imageUrl[i].src = resultFromAPI[i].imageUrl;
-            cameraName[i].innerText = resultFromAPI[i].name;
-            prices[i].innerText = `${resultFromAPI[i].price.toString().slice(0, -2)},00 € `;
-
-            console.log(prices[i].innerText);
-        }
-    })    
+            resultFromAPI = data
+            // console.log(resultFromAPI)
+            for (let i in resultFromAPI) {
+                // console.log(resultFromAPI[i]);
+                description[i].innerText = resultFromAPI[i].description;
+                imageUrl[i].src = resultFromAPI[i].imageUrl;
+                cameraName[i].innerText = resultFromAPI[i].name;
+                prices[i].innerText = `${resultFromAPI[i].price.toString().slice(0, -2)},00 € `;
+                //Pour les prix diviser par 10 et number to fixe
+                // console.log(prices[i].innerText);
+            }
+        })
 }
-
-// const prix = '123456';
-// console.log(prix.slice(1,-2))
-
 CallAPI()
+
+let btnCard = function()
+{
+    let id = this.id
+    localStorage.id = this.id
+    
+}
+document.getElementById('cardOne').onclick = btnCard;
+document.getElementById('cardTwo').onclick = btnCard;
+document.getElementById('cardThree').onclick = btnCard;
+document.getElementById('cardFour').onclick = btnCard;
+document.getElementById('cardFive').onclick = btnCard;
+
+// export default btnCard
