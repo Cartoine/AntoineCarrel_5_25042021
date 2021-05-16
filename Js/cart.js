f1b0d077d18aa2b53220cd882aa02bfc36016cf1
Cartoine <antoine.carrel@orange.fr>
2021-05-17 00:07:21 +0200

    add customer infos in console onclick 'Acheter'

--- Js/cart.js
+++ Js/cart.js
@@ -4,6 +4,9 @@ const price = localStorage.getItem('Price');
 const imgUrl = localStorage.getItem('image');
 const lense = localStorage.getItem('lense');
 
+// const firstName = document.getElementById('validationDefault01').value
+// console.log(firstName)
+
 function setDataStorage() {
   
     document.getElementById('cameraName').innerHTML = cameraName;
@@ -13,3 +16,23 @@ function setDataStorage() {
   
   }
   setDataStorage()
+
+
+const addPaiement = document.getElementById('buy').addEventListener('click', function() {
+    
+    console.log(document.getElementById('validationDefault01').value)
+    console.log(document.getElementById('validationDefault02').value)
+    console.log(document.getElementById('inputAddress').value)
+    console.log(document.getElementById('validationDefault03').value)
+    console.log(document.getElementById('inputEmail4').value)
+})
+//   contact: {
+//     *   firstName: string,
+//     *   lastName: string,
+//     *   address: string,
+//     *   city: string,
+//     *   email: string
+//     * }
+//     * products: [string] <-- array of product _id
+
+//choper L'ID du produit et le mettre dans le local storage et envoyer a l'API
