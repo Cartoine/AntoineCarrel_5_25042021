9f7804ed6e836adb57752f23a91f7af43956772b
Cartoine <antoine.carrel@orange.fr>
2021-05-17 00:14:30 +0200

    create obejct contact onclick 'Acheter'

--- Js/cart.js
+++ Js/cart.js
@@ -6,7 +6,9 @@ const lense = localStorage.getItem('lense');
 
 // const firstName = document.getElementById('validationDefault01').value
 // console.log(firstName)
+const contact = {
 
+}
 function setDataStorage() {
   
     document.getElementById('cameraName').innerHTML = cameraName;
@@ -20,12 +22,23 @@ function setDataStorage() {
 
 const addPaiement = document.getElementById('buy').addEventListener('click', function() {
     
-    console.log(document.getElementById('validationDefault01').value)
-    console.log(document.getElementById('validationDefault02').value)
-    console.log(document.getElementById('inputAddress').value)
-    console.log(document.getElementById('validationDefault03').value)
-    console.log(document.getElementById('inputEmail4').value)
+    // console.log(document.getElementById('validationDefault01').value)
+    // console.log(document.getElementById('validationDefault02').value)
+    // console.log(document.getElementById('inputAddress').value)
+    // console.log(document.getElementById('validationDefault03').value)
+    // console.log(document.getElementById('inputEmail4').value)
+    const contact = {
+        firstName: document.getElementById('validationDefault01').value,
+        lastName: document.getElementById('validationDefault02').value,
+        address: document.getElementById('inputAddress').value,
+        city: document.getElementById('validationDefault03').value,
+        email: document.getElementById('inputEmail4').value
+    }
+    console.log(contact)
 })
+
+
+// console.log(contact)
 //   contact: {
 //     *   firstName: string,
 //     *   lastName: string,
