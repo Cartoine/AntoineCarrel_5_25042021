4a52956e4fd683b7c5ccfbb7d1f218d74c311895
Cartoine <antoine.carrel@orange.fr>
2021-05-16 22:41:41 +0200

    new header, add lenses choise in cart

--- Js/cart.js
+++ Js/cart.js
@@ -2,12 +2,14 @@
 const cameraName = localStorage.getItem('Camera_name');
 const price = localStorage.getItem('Price');
 const imgUrl = localStorage.getItem('image');
+const lense = localStorage.getItem('lense');
 
-function setData() {
+function setDataStorage() {
   
     document.getElementById('cameraName').innerHTML = cameraName;
     document.getElementById('price').innerHTML = price;
     document.getElementById('imageUrl').src = imgUrl;
+    document.getElementById('lense').innerHTML = `${'Objectif : ' + lense}`;
   
   }
-  setData()
+  setDataStorage()
